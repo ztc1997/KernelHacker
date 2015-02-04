eb35205145d4928f82c780ee8896f43541428093
Alex Zhang <ztc2010@live.cn>
2015-02-04 21:20:49 +0800

    添加关于，开放源代码许可

--- app/src/main/java/com/ztc1997/kernelhacker/ui/MiscFragment.java
+++ app/src/main/java/com/ztc1997/kernelhacker/ui/MiscFragment.java
@@ -8,17 +8,29 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.ztc1997.kernelhacker.R;
+import com.ztc1997.kernelhacker.view.PreferenceView;
+
+import de.psdev.licensesdialog.LicensesDialog;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MiscFragment extends Fragment {
+    private PreferenceView licensesView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_misc, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_misc, container, false);
+        licensesView = (PreferenceView) rootView.findViewById(R.id.misc_about_licenses);
+        licensesView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new LicensesDialog.Builder(getActivity()).setNotices(R.raw.notices).setIncludeOwnLicense(true).build().show();
+            }
+        });
+        return rootView;
     }
 
 
